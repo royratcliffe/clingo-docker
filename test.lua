d5b5a1d832f8c02761bcacc1429d103a1fa5a7bd
Roy Ratcliffe <roy@ratcliffe.me>
2026-03-07 17:59:09 +0000

    feat: add initial Lua script for Clingo integration
    Implement a Lua script that sets up a local debugger for VSCode
    and integrates with Clingo to define and solve a simple logic
    program. This includes conditional loading of the debugger and
    output of the solver results.

---
+++ test.lua
@@ -0,0 +1,20 @@
+if os.getenv("LOCAL_LUA_DEBUGGER_VSCODE") == "1" then
+  package.path = os.getenv("LOCAL_LUA_DEBUGGER_PATH") .. ";" .. package.path
+  assert(require("lldebugger")).start()
+end
+
+local clingo = require "clingo"
+local control = clingo.Control()
+control:add("base", {}, [[
+a :- not b.
+b :- not a.
+]])
+control:ground { { "base", {} } }
+local solveresult = control:solve {
+  on_model = function(model)
+    for _, symbol in ipairs(model:symbols { shown = true }) do
+      print(symbol)
+    end
+  end
+}
+print(solveresult)
